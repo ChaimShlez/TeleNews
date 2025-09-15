3a1c8f58b8939c3ed4aa5f3275ea74daf52009d1
shuki <shukiheftler@gmail.com>
2025-09-15 12:31:45 +0300

    saving three collection in mongodb

--- services/DataLoaders/TelegramLoader/admin.py
+++ services/DataLoaders/TelegramLoader/admin.py
@@ -1,3 +1,5 @@
+from tabnanny import check
+
 from utils.mongodb.mongodb_service import MongoDBService
 from services.DataLoaders.TelegramLoader.config import *
 from utils.logger.logger import Logger
@@ -8,35 +10,63 @@ from utils.logger.logger import Logger
 class Admin:
     def __init__(self):
         self.mongodb = MongoDBService(CONNECTION_STRING,DB_NAME)
-    def admin_channels(self):
-        self.mongodb.insert_one(ADMIN_COLLECTION,{"channels":CHANNELS})
+        self.channels = CHANNELS
+    def admin_channels(self,channels=CHANNELS):
+        if isinstance(list,channels):
+            for channel in channels:
+                self.mongodb.insert_one(ADMIN_COLLECTION,{"channel":channel})
+        else:
+            self.mongodb.insert_one(ADMIN_COLLECTION, {"channel": channels})
+
+        self.channels.add(channels)
         # logger.info("default channel added")
     def check_channels(self,channel: str):
-        pass
-        """
-        
-        check channels are !!
-        
-        logger.info("validating the channel contents")
-        #if score:
-            logger.info("channel passed the validation")
-            self.mongodb.insert_one(CHECK_COLLECTION,{"channel":channel})
-        else:
-            logger.info("channel passed to blacklist content is not proper") 
-            self.blacklist_channel(channel)
-        """
+
+        admin_channels = set()
+        blacklist_channels = set()
+        check_channels = set()
+        admin_mongo = self.mongodb.find(ADMIN_COLLECTION,query=None,fields=["channel"])
+        if admin_mongo:
+            for channel,name in admin_mongo.items():
+                admin_channels.add(name)
+
+        blacklist_mongo = self.mongodb.find(BLACKLIST_COLLECTION, query=None, fields=["channel"])
+        if blacklist_mongo:
+            for channel, name in blacklist_mongo.items():
+                blacklist_channels.add(name)
+
+        check_mongo = self.mongodb.find(CHECK_COLLECTION, query=None, fields=["link"])
+        if check_mongo:
+            for channel, name in check_mongo.items():
+                check_channels.add(name)
 
 
+        check_channels = check_channels - admin_channels - blacklist_channels
+
+        for channel in check_channels:
+            """
+            תכניס פה את הלוגיקה של הסקור
+            
 
 
+            # logger.info("validating the channel contents")
+            #if score:
+                # logger.info("channel passed the validation")
+                self.admin_channels(channel)
+            else:
+                # logger.info("channel passed to blacklist content is not proper")
+                self.blacklist_channel(channel)
+            """
 
     def blacklist_channels(self,channel:str):
 
         pass
 
         """
-        logger.info("saving bad channel in mongodb in the blacklist")
+        # logger.info("saving bad channel in mongodb in the blacklist")
         self.mongodb.insert_one(BLACKLIST_COLLECTION,{"channel":channel})
         
         """
+    def get_channels(self) -> set:
+        return self.channels
 
--- services/DataLoaders/TelegramLoader/config.py
+++ services/DataLoaders/TelegramLoader/config.py
@@ -11,12 +11,12 @@ CHAT_ID = int(os.getenv('CHAT_ID'))
 TOPIC = os.getenv("TOPIC","text-telegram")
 REDIS_HOST = os.getenv("REDIS_HOST","redis")
 
-CHANNELS = ["https://t.me/amitsegal","https://t.me/cnn_world_news",
+CHANNELS = {"https://t.me/amitsegal","https://t.me/cnn_world_news",
             "https://t.me/TommyRobinsonNews","https://t.me/rian_ru",
             "https://t.me/indianexpress","https://t.me/voachinese",
             "https://t.me/Tasnimnews","https://t.me/tikvahethiopia",
             "https://t.me/TheBigBadShadow","https://t.me/INSIDER_UK_NEWS",
-            "https://t.me/Sport_HUB_football","https://t.me/sportsdirect_eng"]
+            "https://t.me/Sport_HUB_football","https://t.me/sportsdirect_eng"}
 
 
 CONNECTION_STRING = os.getenv("CONNECTION_STRING" ,"mongodb://TeleNews:TeleNews@mongo:27017/TeleNews?authSource=admin")
