1f1521b299872833cd67142781018bd2da6aaaca
Chaim Shlez <chaimshlez@gmail.com>
2025-09-15 17:13:54 +0300

    update_redis

--- services/ManagerMessage/src/consumer_manager.py
+++ services/ManagerMessage/src/consumer_manager.py
@@ -2,7 +2,8 @@ from services.ManagerMessage.src.message_sender import MessageSender
 from utils.kafka_pub_sub.sub.consumer import Consumer
 from utils.kafka_pub_sub.pub.producer import Producer
 from utils.mongodb.mongodb_service import MongoDBService
-
+from utils.redis.redis_service import RedisService
+from config import *
 
 class ConsumerManager:
 
@@ -12,6 +13,8 @@ class ConsumerManager:
         self.mongo_service = MongoDBService(connection, db_name)
         self.collection = collection
         self.sender = MessageSender(url)
+        self.redis_service = RedisService(host=HOST, port=REDIS_PORT)
+
 
 
     def __get_users_id_from_mongo(self, topic: str, collection: str)-> list[dict]:
@@ -26,6 +29,7 @@ class ConsumerManager:
         print("starting to consume")
         for message in self.events:
             users_id: list[dict] = self.__get_users_id_from_mongo(message.topic, self.collection)
-            response = self.sender.send_message(message.value, users_id)
+            media_data = self.redis_service.get_media_with_metadata(message.value['id'],message.value['metadata']['chat'])
+            response = self.sender.send_message(message.value, users_id,media_data)
             print(response)
 
