4c24be9e141b34bcb7d4ccee41f8c1de61985451
yakov matan <yakovmatan@gmail.com>
2025-09-15 11:48:39 +0300

    create function to fit message

--- services/ManagerMessage/src/message_sender.py
+++ services/ManagerMessage/src/message_sender.py
@@ -5,5 +5,12 @@ class MessageSender:
     def __init__(self, url: str):
         self.url = url
 
+    @staticmethod
+    def fit_message(message: str, users: list[dict]) -> list[tuple[str, str]]:
+        data = [('message', message)]
+        for user in users:
+            data.append(('users_id', str(user["user_id"])))
+        return data
+
 
 
