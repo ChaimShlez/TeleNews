868c3c7f518645650d69b168f57bd4ebd976d651
shuki <shukiheftler@gmail.com>
2025-09-15 10:05:23 +0300

    mongo added to telegram listiner

--- services/TelegramBots/TelegramListener/main.py
+++ services/TelegramBots/TelegramListener/main.py
@@ -1,7 +1,8 @@
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ConversationHandler, ContextTypes, filters
 from config import *
-from topics import TOPICS
+from utils.topics.topics import TOPICS
+from utils.mongodb.mongodb_service import MongoDBService
 TOKEN = TOKEN_BOT
 
 SELECT_TOPICS = 1
@@ -20,7 +21,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if user_id not in user_preferences:
             user_preferences[user_id] = set()
         # מציגים אפשרויות נושאים
-        keyboard = [[topic] for topic in TOPICS]
+        keyboard = [[topic] for topic in TOPICS.keys()]
         await update.message.reply_text(
             "באיזה נושאים אתה רוצה לקבל עדכונים? בחר אחד כל פעם, או כתוב /done כשסיימת.",
             reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
@@ -32,8 +33,8 @@ async def select_topic(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if (update.message is not None)  :
         user_id = update.message.from_user.id
         topic = update.message.text
-        if topic in TOPICS:
-            user_preferences[user_id].add(topic)
+        if topic in TOPICS.keys():
+            user_preferences[user_id].add(TOPICS[topic])
             await update.message.reply_text(f"הוספת את הנושא: {topic}. תוכל לבחור עוד נושא, או /done כדי לסיים.")
         else:
             await update.message.reply_text("הנושא לא מוכר, נסה לבחור מרשימת הכפתורים.")
@@ -47,6 +48,11 @@ async def done(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chosen = user_preferences.get(user_id, set())
         if chosen:
             await update.message.reply_text(f"נרשמת לנושאים: {', '.join(chosen)}")
+            # adding to mongodb
+            mongo = MongoDBService(CONNECTION_STRING,DB_NAME)
+            mongo.insert_one(COLLECTION_NAME,{str(user_id) :chosen})
+
+
         else:
             await update.message.reply_text("לא נבחרו נושאים. תוכל להריץ /start שוב בכל עת.")
         return ConversationHandler.END
