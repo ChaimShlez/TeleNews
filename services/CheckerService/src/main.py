c42247fa99c4b95aa5fb3c5d6c69efcb15353883
Chaim Shlez <chaimshlez@gmail.com>
2025-09-16 15:02:51 +0300

    yaml

--- services/CheckerService/src/main.py
+++ services/CheckerService/src/main.py
@@ -21,4 +21,4 @@ async def check_channel_endpoint(data: ChannelData):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
