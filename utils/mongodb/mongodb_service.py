96c1a088dc5ed3c5e92fc8cf3e5842a7a478871f
Aharon Frenkel <frenkelahron@gmail.com>
2025-09-17 12:34:02 +0300

    refactor: Update mongodb service
    - update the upsert_and_push method: push_value argument type convert to list, and insert the values platen

--- utils/mongodb/mongodb_service.py
+++ utils/mongodb/mongodb_service.py
@@ -65,12 +65,12 @@ class MongoDBService:
         return cursor.to_list()
 
     def upsert_and_push(
-            self ,
-            collection: str ,
-            query: dict[str , Any] ,
-            push_field: str ,
-            push_value: Any
-    ) -> bool:
+            self,
+            collection: str,
+            query: dict[str, Any],
+            push_field: str,
+            push_value: list[Any]
+    ):
         """
         Update a document to push a value to a list field or insert a new document
 
@@ -79,21 +79,19 @@ class MongoDBService:
             query: The query to find the document by (example: {"userid": 123})
             push_field: The field to push the value to (example: "topics")
             push_value: The value to push to the field (example: "politics")
-
-        Returns:
-            True if acknowledged that the document was updated, False otherwise
         """
-        response = self._db[collection].update_one(
-            query ,
-            {"$push": {push_field: push_value}}
-        )
-        if not response.modified_count:
+        if not self.find(collection, query):
             document = {
                 **query ,
-                push_field: [push_value]
+                push_field: [value for value in push_value]
             }
-            response = self.insert_one(collection , document)
-        return response.acknowledged
+            return self.insert_one(collection, document)
+        for value in push_value:
+            self._db[collection].update_one(
+                query ,
+                {"$addToSet": {push_field: value}}
+            )
+        return True
 
     @staticmethod
     def _parse_projection(fields: list[str], exclude_id: bool) -> dict[str, int] | None:
